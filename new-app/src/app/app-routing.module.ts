12b06d2700e1992c51c6d13da94161d366827b49
benjo2021 <benjo2021@yahoo.com>
2018-09-22 16:02:12 +0800

    added error 404 component

--- new-app/src/app/app-routing.module.ts
+++ new-app/src/app/app-routing.module.ts
@@ -4,13 +4,33 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './component/home/home.component';
 import { MainComponent } from './component/main/main.component';
+import { FormComponent } from './component/form/form.component';
+import { Error404Component } from './component/error404/error404.component';
 
 const routes: Routes = [
   {
     path: "home",component : HomeComponent
   },
   {
-    path: "main",component : MainComponent
+    path: "main",component: MainComponent
+     
+    
+  }  ,
+  {
+    path: "main",children:[
+      {
+        path: "main",component:MainComponent
+      },
+      {
+        path: "form",component:FormComponent
+      }
+    ]
+  },
+  {
+    path: "",component : MainComponent
+  },
+  {
+    path: "**",component : Error404Component
   }
 
 ];
--- new-app/src/app/app.module.ts
+++ new-app/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { NavbarComponent } from './component/navbar/navbar.component';
 import { HomeComponent } from './component/home/home.component';
 import { FormComponent } from './component/form/form.component';
 import { MainComponent } from './component/main/main.component';
+import { Error404Component } from './component/error404/error404.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { MainComponent } from './component/main/main.component';
     NavbarComponent,
     HomeComponent,
     FormComponent,
-    MainComponent
+    MainComponent,
+    Error404Component
   ],
   imports: [
     BrowserModule,
